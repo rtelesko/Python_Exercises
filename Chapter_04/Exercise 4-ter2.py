12be0f4605ce454a6281ea07b2518ebfc2e91dd3
rtelesko <rainer.telesko@fhnw.ch>
2026-09-14 14:12:11 +0200

    Removing ter examples

--- Chapter_04/Exercise 4-ter2.py
+++
@@ -1,8 +0,0 @@
-import sympy
-
-number = int(input("Please enter an integer: "))
-
-if sympy.isprime(number):
-    print(number, "is a prime number.")
-else:
-    print(number, "is NOT a prime number.")
--- Chapter_04/Exercise 4-ter3.py
+++
@@ -1,8 +0,0 @@
-# Not a real infinite loop
-# for i in range(10**100):
-#   print(i)
-
-# Real infinite loop
-from itertools import count
-for i in count():
-    print("Value of i: ", i)
