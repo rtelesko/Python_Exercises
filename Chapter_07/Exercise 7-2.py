0f0a608e9972219edb5e353e800a48844ed9ddc6
rtelesko <rainer.telesko@fhnw.ch>
2026-09-22 16:44:22 +0200

    Optimizing Exercise 7-12

--- Chapter_07/Exercise 7-2.py
+++ Chapter_07/Exercise 7-2.py
@@ -4,19 +4,19 @@ import random
 
 def main():
     # Initialize list of numbers.
-    number_list = [0, 0, 0, 0, 0, 0, 0]
+    number_list = [0] * 7
 
     # Assign random numbers to list.
     for i in range(7):
         number_list[i] = random.randint(0, 9)
 
     # Display numbers in a single line.
-    for i in range(7):
-        print (number_list[i], end='')
-        
-        # Separate current number from next number.
-        if i < 6:
-            print(', ', end='')
+    # With square brackets
+    print(number_list, sep=', ')
+    # * = take the items out of the container and pass them individually (unpacking the list)
+    print(*number_list, sep=', ')
+
+
 
 # Call the main function.
 if __name__ == '__main__':
