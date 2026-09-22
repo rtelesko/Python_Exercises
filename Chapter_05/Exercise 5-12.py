5e5d4b4372aa9f4ba89ee44d102035ad69383f2b
rtelesko <rainer.telesko@fhnw.ch>
2026-09-22 15:31:48 +0200

    Update Exercise 5-12

--- Chapter_05/Exercise 5-12.py
+++ Chapter_05/Exercise 5-12.py
@@ -12,14 +12,14 @@ def main():
     # loop on time (in seconds)
     for time in range(1, 11):
         distance = falling_distance(time)
-        print(f'{time}\t{distance:.2f}')
+        print(f'{time}\t\t{distance:.2f}')
 
 # The falling_distance function receives an object's
 # falling time and returns the distance it has fallen
 # in that time
 def falling_distance(time):
-    fallDistance = (9.8 * time * time) / 2
-    return fallDistance
+    fall_distance = (9.8 * time * time) / 2
+    return fall_distance
 
 # Call the main function.
 main()
